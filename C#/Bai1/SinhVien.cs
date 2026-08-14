111cd714b63587f030290f727749e10a3f0e56c9
Hoàng Thu Hằng <hoangduongskj3@gmail.com>
2026-08-14 21:20:14 +0700

    Add files via upload

---
+++ C#/Bai1/SinhVien.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    public class SinhVien
+    {
+        // Thuộc tính (Property)
+        public string MaSV { get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string GioiTinh { get; set; }
+        public string Email { get; set; }
+        public string SDT { get; set; }
+        public string NganhHoc { get; set; }
+        public float DiemTB { get; set; }
+        public string TrangThaiHT { get; set; }
+
+        // Constructor
+        public SinhVien(string maSV, string hoTen, DateTime ngaySinh, string gioiTinh,
+                        string email, string sDT, string nganhHoc, float diemTB, string trangThai)
+        {
+            MaSV = maSV;
+            HoTen = hoTen;
+            NgaySinh = ngaySinh;
+            GioiTinh = gioiTinh;
+            Email = email;
+            SDT = sDT;
+            NganhHoc = nganhHoc;
+            DiemTB = diemTB;
+            TrangThaiHT = trangThai;
+        }
+
+        // Phương thức hiển thị thông tin
+        public void HienThiThongTin()
+        {
+            Console.WriteLine($"Ma SV: {MaSV}");
+            Console.WriteLine($"Ho ten: {HoTen}");
+            Console.WriteLine($"Ngay sinh: {NgaySinh.ToShortDateString()}");
+            Console.WriteLine($"Gioi tinh: {GioiTinh}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"SDT: {SDT}");
+            Console.WriteLine($"Nganh hoc: {NganhHoc}");
+            Console.WriteLine($"Diem TB: {DiemTB}");
+            Console.WriteLine($"Trang thai: {TrangThaiHT}");
+        }
+    }
+}
---
+++ C#/Bai2/NhanVien.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    #region lop nhan vien
+    public class NhanVien
+    {
+        public string MaNV { get; set; }
+        public string HoTen { get; set; }
+        public string PhongBan { get; set; }
+        public DateTime NgayVaoLam { get; set; }
+        public double LuongCB { get; set; }
+
+        public NhanVien(string maNV, string hoTen, string phongBan, DateTime ngayVaoLam, double luongCB)
+        {
+            MaNV = maNV;
+            HoTen = hoTen;
+            PhongBan = phongBan;
+            NgayVaoLam = ngayVaoLam;
+            LuongCB = luongCB;
+        }
+        public void HienThiThongTin()
+        {
+            Console.WriteLine($"Ma nhan vien: {MaNV}");
+            Console.WriteLine($"Ho ten: {HoTen}");
+            Console.WriteLine($"Phong ban: {PhongBan}");
+            Console.WriteLine($"Ngay vao lam: {NgayVaoLam}");
+            Console.WriteLine($"Luong co ban: {LuongCB}");
+        }
+    }
+    #endregion
+
+    #region lop nhan vien chinh thuc
+    public class NVChinhThuc : NhanVien
+    {
+        private long phuCap = 1000000;
+        public NVChinhThuc(string maNV, string hoTen, string phongBan, DateTime ngayVaoLam, long luongCB)
+            : base(maNV, hoTen, phongBan, ngayVaoLam, luongCB)
+        {
+        }
+
+        public long TinhLuong(long luongCB)
+        {
+            return luongCB + phuCap;
+        }
+    }
+    #endregion
+
+    #region lop nhan vien thu viec
+    public class NVThuViec : NhanVien
+    {
+        public NVThuViec(string maNV, string hoTen, string phongBan, DateTime ngayVaoLam, long luongCB)
+            : base(maNV, hoTen, phongBan, ngayVaoLam, luongCB)
+        {
+        }
+        public long TinhLuong(long luongCB)
+        {
+            return (long)(luongCB * 0.85);
+        }
+    }
+
+    public class NVThoiVu : NhanVien
+    {
+        private long soGioLam;
+        private long donGiaGio;
+        public NVThoiVu(string maNV, string hoTen, string phongBan, DateTime ngayVaoLam, long luongCB)
+            : base(maNV, hoTen, phongBan, ngayVaoLam, luongCB)
+        {
+        }
+        public long TinhLuong(long luongCB)
+        {
+            return soGioLam * donGiaGio;
+        }
+    }
+}
